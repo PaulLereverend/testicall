c3a7e416f7e6fa9b96a07ad8f84f963251bf5784
Hugo <hugo.huet.14@gmail.com>
2021-11-25 14:18:51 +0100

    feat(route): init routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { AuthGuard } from './guard/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { 
+    path: '/profile', 
+    component: HomeComponent, 
+    canActivate: [AuthGuard] 
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
